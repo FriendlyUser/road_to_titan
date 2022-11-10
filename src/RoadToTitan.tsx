cf68d8b3fef02dd4fb18a56c2662b64f5d92021a
David Li <davidli012345@gmail.com>
2022-11-10 03:49:59 +0000

    feat: build video

--- src/RoadToTitan.tsx
+++ src/RoadToTitan.tsx
@@ -1,3 +1,4 @@
+import {Audio} from 'remotion'
 import {Video} from 'remotion'
 import {Img, spring, staticFile} from 'remotion';
 import {
@@ -12,6 +13,7 @@ import {Subtitle} from './HelloWorld/Subtitle';
 import {Title} from './HelloWorld/Title';
 import seasonsP1 from '../public/stats/seasons_p1.json'
 import {RankContainer} from './ranks/index';
+import { ScreenshotContainer, SimpleContainer } from './Screenshots';
 
 
 
@@ -23,7 +25,12 @@ export const HyunwooVideo: React.FC<{
 	const frame = useCurrentFrame();
 	const {durationInFrames, fps} = useVideoConfig();
 
-	const openingDuration = 30*20;
+	const openingDuration = fps*20;
+	const buffsNerfsMeanMessageDuration = fps*20;
+	// Length of video / playbackspeed, doubled
+	const medalVideoI = fps*60/2;
+	const medalVideoII = fps*60/2;
+	const twitchClipDuration = fps*21;
 
 	const renderSeasonText = () => {
 		// Center div with transitioning appearing text
@@ -39,14 +46,6 @@ export const HyunwooVideo: React.FC<{
 		const hyunWooStats = seasonData.games[0]
 		const startFrame = seasonToShow * openingDuration / 3;
 		const endFrame = startFrame + openingDuration / 3;
-
-    const bounceAnimation = spring({
-        frame,
-        from: 0,
-        to: 1,
-        fps,
-        config: { damping: 10.5, stiffness: 160, mass: 0.6 },
-    });
 
 		const headingOpacity = interpolate(frame, [startFrame, endFrame], [1, 0.25]);
 
@@ -93,7 +92,83 @@ export const HyunwooVideo: React.FC<{
 		)
 	};
 
-	const renderTwoVideosSeq1 = () => {
+	const renderMedalVidsI = () => {
+		const vidData = [
+			{
+				vidSrc: "vids/1625621478-medaltveternalreturnblacksurvival20210706183010.mp4",
+				playbackSpeed: 4
+			},
+			{
+				vidSrc: "vids/1625789769-medaltveternalreturnblacksurvival20210708171311.mp4",
+				playbackSpeed: 4
+			},
+		]
+		const [vid1, vid2] = vidData;
+		const adjustedFrame = frame - openingDuration - buffsNerfsMeanMessageDuration;
+		const vidsToShow = Math.floor((adjustedFrame / medalVideoI) * 2);
+		let video1;
+		let video2;
+		if (vidsToShow === 0) {
+			video1 = <Video src={staticFile(vid1.vidSrc)} playbackRate={2} style={{
+				width: "50%"
+			}}/>
+			video2 = <Video src={staticFile(vid2.vidSrc)} playbackRate={2} style={{
+				width: "50%"
+			}}/>
+			return (
+				<>
+					<div style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					width: "100%",
+					zIndex: 5,
+					position: "absolute",
+					height: "100%",
+				}}>
+						{video1}
+						{video2}
+				</div>
+				</>
+			);
+		};
+	}
+
+	const renderMedalVidsII = () => {
+		const vidData = [
+			{
+				vidSrc: "vids/1624767187-medaltveternalreturnblacksurvival20210626211107.mp4",
+				playbackSpeed: 4		
+			},
+			{
+				vidSrc: "vids/1636862969-medaltveternalreturnblacksurvival20211113200734-tr-edit.mp4",
+				playbackSpeed: 4
+			},
+		]
+		const [vid1, vid2] = vidData;
+		return (
+			<>
+				<div style={{
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				width: "100%",
+				zIndex: 5,
+				position: "absolute",
+				height: "100%",
+			}}>
+					<Video loop src={staticFile(vid1.vidSrc)} playbackRate={2} style={{
+			width: "50%"
+		}} />
+					<Video loop src={staticFile(vid2.vidSrc)} playbackRate={2} style={{
+			width: "50%"
+		}}/>
+			</div>
+			</>
+		);
+	}
+
+	const renderTwoVideosSeqWin = () => {
 		return (
 			<div style={{
 				display: "flex",
@@ -110,11 +185,89 @@ export const HyunwooVideo: React.FC<{
 				<Video src={staticFile("vids/vod-1645799798-offset-9696.mp4")} style={{
 						width: "50%"
 					}} volume={0}/>
+				<Audio src={staticFile("fankit/hyunwoo_firstMove_1_en.wav")} volume={0.9}/>
 			</div>
 		)
 	}
+
+	const renderScreenshots = () => {
+
+		const screenshotsData = [
+			{
+				imgSrc: "imgs/hyunwoo_nerf_16.png",
+				text: "Hyunwoo nerfed, does no damage now."		
+			},
+			{
+				imgSrc: "imgs/hyunwoo_nerf_again.png",
+				text: "Hyunwoo nerfed again, cant burst people now."
+			},
+			{
+				imgSrc: "imgs/hyunwoo_nerf40_0.png",
+				text: "Hyunwoo nerfed again, cant burst people now 2."
+			},
+			{
+				imgSrc: "imgs/bulliedByGamer.png",
+				text: "Bad gamer here cant win erbs.",
+				audioSrc: "fankit/hyunwoo_firstMove_2_en.wav"
+			}
+		]
+		const adjustedFrame = frame - openingDuration;
+		const screenToShow = Math.floor((adjustedFrame / buffsNerfsMeanMessageDuration) * screenshotsData.length);
+
+		const screenshotInfo = screenshotsData[screenToShow];
+		const startFrame = screenToShow * buffsNerfsMeanMessageDuration / screenshotsData.length;
+		if (!screenshotInfo) {
+			return null;
+		}
+		return (
+			<>	
+					<ScreenshotContainer img={screenshotInfo.imgSrc} text={screenshotInfo.text} delay={startFrame}/>
+					<Audio loop src={staticFile("fankit/hyunwoo_firstMove_2_en.wav")} volume={1}/>
+			</>
+		);
+	}
+
+	const renderShowTitanLevel = () => {
+		const headingOpacity = interpolate(frame, [0, 10*fps], [1, 0.25]);
+		return (
+			<>
+			<div style={{
+				position: "absolute",
+				zIndex: 5,
+				width: "100%",
+				}}>
+					<h1 style={{
+						opacity: headingOpacity,
+						fontWeight: 'bold',
+						fontSize: 100,
+						textAlign: 'center',
+						position: 'absolute',
+						top: 100,
+						width: '100%',
+						// Transform: 'translateY(-50%)',
+					}}>Season 7</h1>
+						<h6 
+							style={{
+								color: titleColor,
+								fontSize: 60,
+								textAlign: 'center',
+								position: 'absolute',
+								top: 300,
+								width: '100%',
+								// Opacity: (endFrame - startFrame) / (frameDuration),
+								// Transform: `scale(${bounceAnimation})`,
+							}}>
+								336 Games
+						</h6>
+						<RankContainer tier="titan" subdivision="" naRank="24" delay={0}/>
+				</div>
+			</>
+		)
+	};
+
 	return (
 		<>
+			<Audio volume={0.19} src={staticFile("fankit/er_instrumental.mp3")} />
 				{/* A bunch of stuff rendered to show the color stuff */}
 				<Sequence durationInFrames={openingDuration}>
 					<AbsoluteFill>
@@ -123,8 +276,37 @@ export const HyunwooVideo: React.FC<{
 					{renderSeasonText()}
 				</Sequence>
 
-				<Sequence from={openingDuration} durationInFrames={30*21}>
-					{renderTwoVideosSeq1()}
+				<Sequence from={openingDuration} durationInFrames={buffsNerfsMeanMessageDuration}>
+					{renderScreenshots()}
+				</Sequence>
+
+				<Sequence from={openingDuration +buffsNerfsMeanMessageDuration} durationInFrames={medalVideoI}>
+					{renderMedalVidsI()}
+					<AbsoluteFill style={{
+						zIndex: 1,
+					}}>
+						{renderBg("fankit/ER_3840x2160.png", 0.5)}
+					</AbsoluteFill>
+				</Sequence>
+				<Sequence from={openingDuration +buffsNerfsMeanMessageDuration+ medalVideoI} durationInFrames={medalVideoI}>
+					{renderMedalVidsII()}
+					<AbsoluteFill style={{
+						zIndex: 1,
+					}}>
+						{renderBg("fankit/ER_3840x2160.png", 0.5)}
+					</AbsoluteFill>
+				</Sequence>
+
+				<Sequence from={openingDuration+buffsNerfsMeanMessageDuration+medalVideoI*2} durationInFrames={twitchClipDuration}>
+					{renderTwoVideosSeqWin()}
+					<AbsoluteFill style={{
+						zIndex: 1,
+					}}>
+						{renderBg("fankit/ER_3840x2160.png", 0.5)}
+					</AbsoluteFill>
+				</Sequence>
+				<Sequence from={openingDuration+buffsNerfsMeanMessageDuration+medalVideoI*2+twitchClipDuration} durationInFrames={fps*10}>
+					{renderShowTitanLevel()}
 					<AbsoluteFill style={{
 						zIndex: 1,
 					}}>
--- src/Screenshots.tsx
+++ src/Screenshots.tsx
@@ -13,42 +13,56 @@ export const useAppearWithScaleAndBounce = (delay = 0): { scaleValue: number } =
       from: 0,
       to: 1,
       fps,
-      config: { damping: 10.5, stiffness: 160, mass: 0.6 },
+      config: { damping: 19.5, stiffness: 200, mass: 0.3 },
   });
 
   return { scaleValue: bounceAnimation };
 };
 
-type BounceContainerProps = {
+type ScreenshotContainerProps = {
   // Children?: React.ReactNode;
-  tier: string;
-  subdivision: string;
-  naRank?: string | null
+  img: string;
+  text: string;
   delay: number;
 };
 
-export const RankContainer: React.FC<BounceContainerProps> = ({tier, subdivision, naRank, delay}) => {
+export const ScreenshotContainer: React.FC<ScreenshotContainerProps> = ({img, text, delay}) => {
   const { scaleValue } = useAppearWithScaleAndBounce(delay);
-  const rankImgUrl = `fankit/${tier.toLowerCase()}.png`
-
-  const rankData = naRank ? ` / ${naRank}` : null
+  const rankImgUrl = img
   return (
       <Container $scale={scaleValue} style={{
         position: "absolute",
-        top: 400,
         width: "100%",
       }}>
         <Img src={staticFile(rankImgUrl)} style={{
-          height: "400px",
+          height: "800px",
         }} />
         <h4 style={{
-          fontSize: "20px",
+          fontSize: "40px",
         }}>
-          {tier} {subdivision || ""} {rankData}
+          {text}
         </h4>
       </Container>
   );
 };
+
+export type Props = {
+  children?: React.ReactNode;
+  delay?: number;
+}
+export const SimpleContainer = ({children, delay}: Props) => {
+  const { scaleValue } = useAppearWithScaleAndBounce(delay);
+  return (
+    <>
+      <Container $scale={scaleValue} style={{
+        position: "absolute",
+        width: "100%",
+      }}>
+        {children}
+      </Container>
+    </>
+  );
+}
 
 const Container = styled.div<{ $scale: number }>`
   align-items: center;
